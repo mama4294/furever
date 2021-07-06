f95d6201c21d448ba10526c3894ce6577cea5c84
Matthew Malone <mama4294@colorado.edu>
2021-07-05 21:13:49 -0600

    Initial Commit

---
+++ Furever/FureverApp.swift
@@ -0,0 +1,17 @@
+//
+//  FureverApp.swift
+//  Furever
+//
+//  Created by Matthew Malone on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct FureverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
